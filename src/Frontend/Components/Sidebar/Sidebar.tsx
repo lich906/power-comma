9be236f33c724d482fa6643e00b0dab719706e35
AndreyM <222mushkin@gmail.com>
2021-11-16 22:04:04 +0300

    5-View Добавил выделение нескольких элементов через shift в сайдбаре

--- src/Frontend/Components/Sidebar/Sidebar.tsx
+++ src/Frontend/Components/Sidebar/Sidebar.tsx
@@ -11,23 +11,39 @@ type SidebarItemProps = {
 }
 
 function Sidebar(props: SidebarProps) {
-    const [selectedItemId, setSelectedItemId] = useState(0);
-
-    function handleSelection(id: number) {
-        setSelectedItemId(id);
-    }
+    const [selectedItemIds, setSelectedItemIds] = useState([1]);
 
     function SidebarItem(props: SidebarItemProps) {
 
+        function handleSelection(e: any) {
+            let low, high: number;
+            let range: number[] = [];
+            if (e.shiftKey) {
+                if (selectedItemIds[0] < props.id) {
+                    low = selectedItemIds[0];
+                    high = props.id;
+                } else {
+                    low = props.id;
+                    high = selectedItemIds[0];
+                }
+                for (let i = low; i <= high; i++) {
+                    range.push(i);
+                }
+                setSelectedItemIds(selectedItemIds => range);
+            } else {
+                setSelectedItemIds(selectedItemIds => [props.id]);
+            }
+        }
+
         if (props.selected) {
             return (
-                <div className="sidebar__item sidebar__item_selected" onClick={() => handleSelection(props.id)}>
+                <div className="sidebar__item sidebar__item_selected" onClick={handleSelection}>
                     {props.id}
                 </div>
             )
         } else {
             return (
-                <div className="sidebar__item" onClick={() => handleSelection(props.id)}>
+                <div className="sidebar__item" onClick={handleSelection}>
                     {props.id}
                 </div>
             )
@@ -36,7 +52,7 @@ function Sidebar(props: SidebarProps) {
 
     let items: JSX.Element[] = [];
     for (let i = 0; i < props.items.length; i++) {
-        let selected = props.items[i].selected || props.items[i].id === selectedItemId;
+        let selected = props.items[i].selected || selectedItemIds.includes(props.items[i].id);
         items.push(<SidebarItem id={props.items[i].id} selected={selected}/>)
     }
 
